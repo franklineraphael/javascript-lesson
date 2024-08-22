9c07768cbda6b74804bc5276bf41995677fbd8cd
franklineraphael45 <franklineraphael45@gmail.com>
2024-08-22 17:15:04 +0100

    java

---
+++ main.js
@@ -0,0 +1,222 @@
+// console.log(9 * 9);
+// console.log("hello world");
+// var visitorname = prompt("what is your name");
+// console.log(visitorname);
+// alert(visitorname);
+// var acceptedterms = confirm("Do you accept the terms and conditions?");
+// console.log(acceptedterms);
+// alert(acceptedterms);
+
+// var knowledge = "guy on braids";
+// console.log(knowledge);
+// console.log(typeof knowledge);
+
+// var invincible = {
+//   name: "frankline",
+//   nickname: "invincible",
+//   occupation: "software developer",
+//   isStudent: true,
+//   age: 2024 - 1990,
+// };
+
+// console.log(invincible);
+// console.log(invincible.name);
+
+// var a = 10;
+// var b = 20;
+// var c = 5;
+// var d = a + b;
+// var e = 20;
+
+// console.log(e == b);
+// console.log(a == b);
+// console.log(a > b);
+// console.log(a != b);
+// console.log(a === b);
+// console.log(a !== b);
+// console.log(a >= b);
+// console.log(a <= b);
+
+// var year = prompt("what year were you born");
+// var age = 2024 - year;
+// console.log("you are" + " " + age + "years old");
+// alert(2024 - year);
+
+// var dollars = prompt("how many dollars are you converting");
+// var naira = 1600 * dollars;
+// console.log("$" + dollars + " is " + "N" + naira);
+// alert("$" + dollars + " is " + "N" + naira);
+// document.getElementById.innerhtml = "hello javascript";
+
+// console.log(5 == 5);
+// console.log(typeof (5 == 5));
+
+// var number = 5 + 5;
+// console.log(number);
+// console.log(typeof number);
+
+// var string = "happy weekend";
+// console.log(string);
+// console.log(typeof string);
+
+// console.log();
+// console.log(typeof null);
+
+// var name = "my name is frankline";
+// var live = "I live in";
+// var address = "lekki lagos";
+// var space = " ";
+
+// console.log(name + space + live + space + address);
+
+// function multiply(a, b, c) {
+//   return a * b * c;
+// }
+// console.log(multiply(40, 40, 40));
+// console.log(typeof multiply);
+
+// function checkIfnumber(x) {
+//   if (typeof x === "number") {
+//     console.log(x + " is a number. ");
+//   }
+// }
+
+// function checkIfnumber(x) {
+//   if (typeof x == "number") {
+//     console.log(x + "is a number.");
+//   }
+// }
+
+// checkIfnumber(5);
+
+// function checkIfnumber(x) {
+//   if (typeof x == "boolean") {
+//     console.log(x + "is a boolean.");
+//   } else {
+//     console.log("oops that's not a number");
+//   }
+// }
+
+// checkIfnumber(nigga);
+
+// function decodecolor(code){
+//   switched(code){
+//     case1:
+//       console.log("red");
+//       break;
+//     case2:
+//       console.log("red");
+//       break;
+//     case3:
+//       console.log("red");
+//       break;
+//   }
+// }
+
+// var number = [10, 20, 30];
+
+// var sum = 0;
+// var i = 0;
+
+// while ((1 < Numbers, length)) {
+//   sum += numbers(i);
+//   // i += 1;
+// }
+
+// var classRoom = ["abimbola", "frankline", "knowledge", "emeka"];
+// console.log(classRoom[5]);
+// console.log(classRoom.length);
+
+// var daysOfTheWeek = [
+//   "monday",
+//   "tuesday",
+//   "wednesday",
+//   "thursday",
+//   "friday",
+//   "saturday",
+// ];
+// console.log(daysOfTheWeek[7]);
+// console.log(daysOfTheWeek.length);
+
+// daysOfTheWeek.push("sunday");
+// console.log(daysOfTheWeek);
+// console.log(daysOfTheWeek.length);
+
+// daysOfTheWeek.unshift("frankline");
+// console.log(daysOfTheWeek);
+// console.log(daysOfTheWeek);
+
+// alert("goodmorning");
+// alert("Frankline");
+// console.log(10 + 5);
+// console.log(20 - 5);
+// // console.log(2 + 2 - 5);
+
+// document.body.innerHTML = "";
+
+// var Tshirt = 10;
+// var socks = 8;
+// var dinner = 20;
+// console.log(Tshirt + socks + dinner);
+
+// var bankaccount = 100;
+// var lunch = 20;
+// var dinner = 50;
+// var job = 200;
+// console.log(bankaccount - dinner - lunch + job);
+
+// var soup = 10;
+// var burgers = 3 * 8;
+// var iceCream = 5;
+// var total = (soup + burgers + iceCream) / 3;
+
+// console.log(soup + burgers + iceCream);
+// console.log(total);
+// // console.log(total / 3);
+
+// var Toaster = 18.5;
+// var shirts = 7.5 * 2;
+// var tax = 10 / 100;
+// var Tax = 20 / 100;
+// var sum = Toaster + shirts;
+// console.log(Toaster + shirts);
+// console.log(tax * sum);
+// console.log(Tax * sum);
+
+// var a = "my is ";
+// var b = "frankline ";
+// var coffee = 5;
+// var bagel = 3;
+// var sign = "Total cost:$";
+// console.log(a + b);
+// console.log(coffee + bagel);
+// var total = coffee + bagel;
+// console.log(sign + total);
+
+// alert("Total cost:$8");
+
+// var coffee = 5.99;
+// var bagel = 2.95;
+// var sign = "Total cost:$";
+// var sum = coffee + bagel;
+// var sum = sum.toFixed(2);
+// console.log(sign + sum);
+
+// alert(`total cost: $${total} Thank you, come again!`);
+
+// var hour = 1;
+// var name = "Frankline";
+
+// if (hour >= 1 && hour <= 12) {
+//   console.log(`good morning ${name}`);
+// } else if (hour >= 13 && hour <= 17) {
+//   console.log(`good afternoon ${name}`);
+// } else {
+//   console.log(`goodnight ${name}`);
+// }
+// var age = 6;
+// if (age <= 6) {
+//   console.log(`discount`);
+// } else if (age > 6) {
+//   console.log(`no discount`);
+// }
